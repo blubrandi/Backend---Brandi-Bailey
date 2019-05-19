ff3d7ebcfae144326905d3b2f72d10754851e724
blubrandi <blubrandi0@gmail.com>
2019-05-19 14:25:41 -0600

    Refactored code that was creating a migrate:latest error.

--- data/migrations/20190519121527_bootstrap.js
+++ data/migrations/20190519121527_bootstrap.js
@@ -5,43 +5,36 @@ exports.up = function (knex, Promise) {
         .createTable('volunteers', tbl => {
             tbl.increments()
 
-            tbl
-                .string('volunteer_username', 128).unique().notNullable()
-                .string('volunteer_password', 528).notNullable()
-                .string('volunteer_name', 128).notNullable()
-                .string('volunteer_email', 128).unique().notNullable()
-                .string('volunteer_phone', 128).unique().notNullable()
+            tbl.string('volunteer_username', 128).unique().notNullable()
+            tbl.string('volunteer_password', 528).notNullable()
+            tbl.string('volunteer_name', 128).notNullable()
+            tbl.string('volunteer_email', 128).unique().notNullable()
+            tbl.string('volunteer_phone', 128).unique().notNullable()
         })
 
         .createTable('businesses', tbl => {
             tbl.increments()
 
-            tbl
-                .string('business_username', 128).unique().notNullable()
-                .string('business_password', 528).notNullable()
-                .string('business_email', 128).unique()
-                .string('business_address', 528).notNullable()
-                .string('business_phone', 128).notNullable()
-                .string('business_contact_person', 128).notNullable()
+            tbl.string('business_username', 128).unique().notNullable()
+            tbl.string('business_password', 528).notNullable()
+            tbl.string('business_email', 128).unique()
+            tbl.string('business_address', 528).notNullable()
+            tbl.string('business_phone', 128).notNullable()
+            tbl.string('business_contact_person', 128).notNullable()
         })
 
-        .create('requests', tbl => {
+        .createTable('requests', tbl => {
             tbl.increments()
 
-            tbl
-                .timestamp('time_created', { useTZ: true }, { precision: 6 }).defaultTo(knex.fn.now(6))
-                .string('request_title', 256).notNullable()
-                .string('request_desc', 528).notNullable()
-                .boolean('completed').notNullable()
-                .string('request_expires_date').notNullable()
-                .date('request_expires_date').notNullable()
-                .time('request_expires_time').notNullable()
+            tbl.timestamp('time_created', { useTZ: true })
+            tbl.string('request_title', 256).notNullable()
+            tbl.string('request_desc', 528).notNullable()
+            tbl.boolean('completed').notNullable()
+            tbl.string('request_expires_date').notNullable()
+            tbl.string('request_expires_time').notNullable()
 
-            tbl
-                .string('volunteer_assigned').unsigned().references('id').inTable('volunteers').onDelete('RESTRICT').onUpdate('CASCADE')
-
-            tbl
-                .string('business_requesting').unsigned().references('id').inTable('businesses').onDelete('RESTRICT').onUpdate('CASCADE')
+            tbl.string('volunteer_assigned').unsigned().references('id').inTable('volunteers').onDelete('RESTRICT').onUpdate('CASCADE')
+            tbl.string('business_requesting').unsigned().references('id').inTable('businesses').onDelete('RESTRICT').onUpdate('CASCADE')
 
         })
 
