669c9a35402e2833857c211468dfba9990d899e3
blubrandi <blubrandi0@gmail.com>
2019-05-19 11:11:15 -0600

    Added port info in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const server = require('./api/server.js')
+
+const port = process.env.PORT || 4200
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}, let's get this y'all!`)
+})
