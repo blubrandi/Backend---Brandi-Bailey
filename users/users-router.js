805fe2fb74ecb91505ae00e713e4282b1a0b1c5c
blubrandi <blubrandi0@gmail.com>
2019-05-21 14:13:28 -0600

    Added route to delete user

--- users/users-router.js
+++ users/users-router.js
@@ -24,11 +24,21 @@ router.put('/:id', restricted, (req, res) => {
     const { username, password, business_name, contact_name, email, phone, address, user_type } = req.body
     Users.update(id, { username, password, business_name, contact_name, address, user_type })
         .then(user => {
-            res.json(user);
+            res.json(user)
         })
         .catch(error => {
-            res.status(500).json({ error: error, message: "Cannot update user" });
+            res.status(500).json({ error: error, message: 'Cannot update user' })
         });
-});
+})
+
+router.delete('/:id', restricted, (req, res) => {
+    Users.remove(req.params.id)
+        .then(user => {
+            res.status(200).json({ message: 'User has been deleted' })
+        })
+        .catch(error => {
+            res.status(500).json({ error, message: 'Error deleting user.' })
+        })
+})
 
 module.exports = router
