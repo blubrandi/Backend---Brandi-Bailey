a6752011b4de4c167020995bcb75af077a80ff42
blubrandi <blubrandi0@gmail.com>
2019-05-22 12:47:06 -0600

    heroku commit

--- data/migrations/20190519121527_bootstrap.js
+++ data/migrations/20190519121527_bootstrap.js
@@ -21,8 +21,8 @@ exports.up = function (knex, Promise) {
 
             tbl.string("request_title", 256).notNullable()
             tbl.string("request_desc", 528).notNullable()
-            tbl.string("volunteer_assigned").unsigned().references("id").inTable("users").onDelete("RESTRICT").onUpdate("CASCADE")
-            tbl.string("business_requesting").unsigned().references("id").inTable("users").onDelete("RESTRICT").onUpdate("CASCADE")
+            tbl.integer("volunteer_assigned").unsigned().references("id").inTable("users").onDelete("RESTRICT").onUpdate("CASCADE")
+            tbl.integer("business_requesting").unsigned().references("id").inTable("users").onDelete("RESTRICT").onUpdate("CASCADE")
             tbl.boolean("completed").notNullable()
             tbl.string("request_expires_date").notNullable()
             tbl.string("request_expires_time").notNullable()
