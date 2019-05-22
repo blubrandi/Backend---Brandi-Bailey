9d6056dfd8df2afbf67daeec28f24d2d615f05c0
blubrandi <blubrandi0@gmail.com>
2019-05-22 12:10:11 -0600

    Added id in string

--- data/migrations/20190519121527_bootstrap.js
+++ data/migrations/20190519121527_bootstrap.js
@@ -25,8 +25,8 @@ exports.up = function (knex, Promise) {
             tbl.string('request_expires_date').notNullable()
             tbl.string('request_expires_time').notNullable()
 
-            tbl.integer('volunteer_assigned').unsigned().references(id).inTable('users').onDelete('RESTRICT').onUpdate('CASCADE')
-            tbl.integer('business_requesting').unsigned().references(id).inTable('users').onDelete('RESTRICT').onUpdate('CASCADE')
+            tbl.integer('volunteer_assigned').unsigned().references('id').inTable('users').onDelete('RESTRICT').onUpdate('CASCADE')
+            tbl.integer('business_requesting').unsigned().references('id').inTable('users').onDelete('RESTRICT').onUpdate('CASCADE')
 
         })
 
