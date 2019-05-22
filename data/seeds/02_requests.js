6b8619032dfe2aaf30d4f425bc663ab8d9b3eb48
blubrandi <blubrandi0@gmail.com>
2019-05-22 09:39:33 -0600

    Ran seed files to send integers rather than strings for ids

--- data/seeds/02_requests.js
+++ data/seeds/02_requests.js
@@ -9,7 +9,7 @@ exports.seed = function (knex, Promise) {
           request_title: 'Soup and sandwiches, pasta, and salad.',
           request_desc: 'Extra tomato soup, vegetable beef soup.  Various sandwiches.  Extra fettucini/alfredo and spaghetti',
           volunteer_assigned: '',
-          business_requesting: '2',
+          business_requesting: 2,
           completed: false,
           request_expires_date: '2019-05-21',
           request_expires_time: '19:45:00'
@@ -17,8 +17,8 @@ exports.seed = function (knex, Promise) {
         {
           request_title: 'BBQ Ribs',
           request_desc: 'Leftover BBQ Ribs from lunch',
-          volunteer_assigned: '1',
-          business_requesting: '1',
+          volunteer_assigned: 1,
+          business_requesting: 1,
           completed: false,
           request_expires_date: '2019-05-21',
           request_expires_time: '18:00:00'
@@ -26,8 +26,8 @@ exports.seed = function (knex, Promise) {
         {
           request_title: 'A lot of stuff from dinner service',
           request_desc: 'different ingredients and stuff from dinner',
-          volunteer_assigned: '2',
-          business_requesting: '3',
+          volunteer_assigned: 2,
+          business_requesting: 3,
           completed: true,
           request_expires_date: '2019-05-21',
           request_expires_time: '22:30:00'
