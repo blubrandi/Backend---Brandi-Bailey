15fd6a16e105884afce998d9c218f608c00f1f37
blubrandi <blubrandi0@gmail.com>
2019-05-23 08:43:27 -0600

    Added additional api documentation, still in progress.  Added auth middleware back into endpoints to protect routes to continue with testing.

--- api/api-routes.js
+++ api/api-routes.js
@@ -2,6 +2,7 @@
  * @api {post} https://replate-lambda.herokuapp.com/auth/register
  * New User Registration
  * @apiGroup Authentication
+ * @apiDescription This is the information that is passed to and returned from the database upon user registration.  Please note that the `user_type` will be passed through to db by frontend and will not be visible/selectable by registering user.  Options are: `volunteer` or `business`.
  * @apiSuccess {integer} id Automatically Increments
  * @apiSuccess {string} username User must provide username on account creation.  Required.
  * @apiSuccess {string} password User must provide password on account creation.  Returns hashed.  Required.
@@ -9,6 +10,7 @@
  * @apiSuccess {string} contact_name User must provide a contact name on account creation.  Required.
  * @apiSuccess {string} address User can provide an address on account creation, if they're a business.  Not Required since volunteers will not need to provide an address.
  * @apiSuccess {string} phone User must provide a phone number on account creation.  Required.
+ * @apiSuccess {string} user_type This is required for registration.  Required.
  * @apiSuccess {string} jsonwebtoken Will be returned by backend upon account creation.
  * @apiSuccessExample {Array} Success-Response:
  *  201 Created
@@ -30,6 +32,7 @@
 * @api {post} https://replate-lambda.herokuapp.com/auth/login
 * User Login
 * @apiGroup Authentication
+* @apiDescription This endpoint allows a registered user to login.  A `username` and `password` is required for successful authentication, which will return a welcome message with `user_name` and the authentication `token`.
 * @apiSuccess {string} username Must be provided upon user login.  Required.
 * @apiSuccess {string} password Must be provided upon user login.  Required.
 * @apiSuccess {string} message Greeting returned from backend upon successful login.  Welcome + contact_name.
@@ -45,6 +48,7 @@
 /**
 * @api {get} https://replate-lambda.herokuapp.com/requests
 * Get All Requests, restricted
+* @apiDescription Here is some text here.  Does this get us anywhere?
 * @apiGroup Requests
 * @apiSuccess {integer} id Automatically Increments
 * @apiSuccess {string} request_title Title of request, submitted by business.  Field required.
@@ -52,30 +56,31 @@
 * @apiSuccess {boolean} completed Indicates if request has been completed by assigned volunteer.  Required.
 * @apiSuccess {date} request_expires_date Allows business to indicate date request will expire.
 * @apiSuccess {time} request_expires_time Allows business to indicate time request will expire.
-* @apiSuccess {integer} volunteer_assigned Indicates which volunteer is assigned, by ID, if a volunteer has claimed the request.
-* @apiSuccess {integer} business_requesting Indicates which business has submitted request, by ID.  Required.
+* @apiSuccess {object} volunteer_assigned Indicates which volunteer is assigned, and provides their `contact_name` and `phone`, if a volunteer has claimed the request.
+* @apiSuccess {object} business_requesting Indicates which business has submitted request, populates automatically.  Returns the `business_name`, `address`, `phone`, `contact_name`, and `email` of the business that submitted the request.
 * @apiSuccessExample {Array} Success-Response:
 *   200 OK
+*
 *  {
-*    "id": 1,
-*    "request_title": "Soup and sandwiches, pasta, and salad.",
-*    "request_desc": "Extra tomato soup, vegetable beef soup.  Various sandwiches.  Extra fettucini/alfredo and spaghetti",
-*    "volunteer_assigned": "",
-*    "business_requesting": "2",
-*    "completed": 1,
+*    "id": 2,
+*    "request_title": "BBQ Ribs and more",
+*    "request_desc": "Leftover BBQ Ribs from lunch",
+*    "completed": 0,
 *    "request_expires_date": "2019-05-21",
-*    "request_expires_time": "19:45:00"
-*  },
-*  {
-*    "id": 3,
-*    "request_title": "A lot of stuff from dinner service",
-*    "request_desc": "different ingredients and stuff from dinner",
-*    "volunteer_assigned": "2",
-*    "business_requesting": "3",
-*    "completed": 1,
-*    "request_expires_date": "2019-05-21",
-*    "request_expires_time": "22:30:00"
+*    "request_expires_time": "18:00:00",
+*    "volunteer_assigned": {
+*      "contact_name": "Tommy",
+*      "phone": "505-231-4567"
+*    },
+*    "business_requesting": {
+*      "business_name": "Dennys",
+*      "address": "123 Adobe Lane NW, Albuquerque, NM 87114",
+*      "phone": "505-123-4567",
+*      "contact_name": "Penny",
+*      "email": "abq_dennys@test.com"
+*    }
 *  }
+*
 * @apiError 401 User Not Authorized to Get Requests
 * @apiErrorExample Error-Response:
 * 401 Not Authorized
@@ -93,8 +98,6 @@
 * @apiSuccess {boolean} completed Indicates if request has been completed by assigned volunteer.  Required.
 * @apiSuccess {date} request_expires_date Allows business to indicate date request will expire.
 * @apiSuccess {time} request_expires_time Allows business to indicate time request will expire.
-* @apiSuccess {integer} volunteer_assigned Indicates which volunteer is assigned, by ID, if a volunteer has claimed the request.
-* @apiSuccess {integer} business_requesting Indicates which business has submitted request, by ID.  Required.
 * @apiSuccessExample {Array} Success-Response:
 *   200 OK
 *  {
--- requests/requests-router.js
+++ requests/requests-router.js
@@ -3,9 +3,9 @@ const router = require('express').Router()
 
 const Requests = require('../requests/requests-model.js')
 const Users = require('../users/users-model.js')
-// const restricted = require('../auth/restricted.js')
+const restricted = require('../auth/restricted.js')
 
-router.get('/', async (req, res) => {
+router.get('/', restricted, async (req, res) => {
     const requests = []
 
     try {
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', restricted, async (req, res) => {
 
     try {
         const request = await Requests.findById(req.params.id)
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', restricted, (req, res) => {
     const { id } = req.params
     const { request_title, request_desc, volunteer_assigned, completed, request_expires_date, request_expires_time } = req.body
     Requests.update(id, { request_title, request_desc, volunteer_assigned, completed, request_expires_date, request_expires_time })
@@ -46,7 +46,7 @@ router.put('/:id', (req, res) => {
         })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', restricted, (req, res) => {
     Requests.remove(req.params.id)
         .then(request => {
             res.status(200).json({ message: 'Request has been deleted' })
